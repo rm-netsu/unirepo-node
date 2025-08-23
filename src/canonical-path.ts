b09c84ced35488225521d8ac094073329f100304
rm-netsu <netsu.code@gmail.com>
2025-08-23 04:23:19 +0300

    extract getFileHashString

--- src/canonical-path.ts
+++ src/canonical-path.ts
@@ -2,20 +2,11 @@ import * as crypto from 'node:crypto'
 import { createReadStream } from 'node:fs'
 import path from 'node:path'
 
-/**
- * Calculates a content hash of a file and returns its canonical path.
- *
- * @param {string} filePath The path to the file.
- * @param {string} hashName The name of the hashing algorithm to use (e.g., 'sha256', 'md5'). Defaults to 'sha256'.
- * @param {string} repoRoot The root directory for the canonical paths.
- * @returns {Promise<string>} A promise that resolves to the canonical path string.
- */
-export const getCanonicalPath = async (
+export const getFileHashString = async (
 	filePath: string,
-	hashName: string = 'sha256',
-	repoRoot: string,
+	builtinHashName: string = 'sha256',
 ): Promise<string> => {
-	const hash = crypto.createHash(hashName)
+	const hash = crypto.createHash(builtinHashName)
 	const fileStream = createReadStream(filePath)
 
 	await new Promise<void>((resolve, reject) => {
@@ -24,11 +15,19 @@ export const getCanonicalPath = async (
 		fileStream.on('error', error => reject(error))
 	})
 
-	const fileHash = hash.digest('hex')
+	return hash.digest('hex')
+}
+
+export const getCanonicalPath = async (
+	filePath: string,
+	builtinHashName: string = 'sha256',
+	repoRootPath: string,
+): Promise<string> => {
+	const fileHash = await getFileHashString(filePath, builtinHashName)
 	const fileExtension = path.extname(filePath).slice(1) // Remove the leading dot
 
 	const canonicalPath = path.join(
-		repoRoot,
+		repoRootPath,
 		fileExtension,
 		fileHash.slice(0, 2),
 		`${fileHash}.${fileExtension}`,
